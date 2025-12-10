07090e236c8ca0cf057b35d8b0e148f11c0db102
Luis Andrés <andrechi20@hotmail.com>
2025-12-09 23:12:28 -0600

    second commit

--- app.py
+++ app.py
@@ -1,39 +1,5 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "1fa30b78-c34e-442b-80ca-359aa2374676",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import streamlit as st\n",
-    "\n",
-    "st.header('Lanzar una moneda')\n",
-    "\n",
-    "st.write('Esta aplicación aún no es funcional. En construcción.')"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python [conda env:base] *",
-   "language": "python",
-   "name": "conda-base-py"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.13.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import streamlit as st
+
+st.header('Lanzar una moneda')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
